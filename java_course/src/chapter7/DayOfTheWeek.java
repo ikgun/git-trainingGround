708b854226b851ed3a449c7d3cf726f04279d805
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-03 16:28:27 +0100

    Completed chapter 7 exercise

---
+++ java_course/src/chapter7/DayOfTheWeek.java
@@ -0,0 +1,30 @@
+package chapter7;
+
+/*
+Make an array that holds the textual values of the days of the week.
+Have the user input a number corresponding to the day of the week.
+Assume the week starts on Monday.
+Your program should output the String that represents the day of the week
+Example: User inputs the number 1, your program should print "Monday"
+ */
+
+import java.util.Scanner;
+
+public class DayOfTheWeek {
+
+    public static void main(String[] args) {
+
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        System.out.println("Enter a number between 1-7 to print a day of the week:");
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Day : " + daysOfWeek[number - 1]);
+
+
+
+    }
+}
