592be68210d5f2802f68fd5e2e7e533db7a2a4aa
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 09:54:34 +0100

    Updated Employee class

--- java_course/src/chapter9/Employee.java
+++ java_course/src/chapter9/Employee.java
@@ -1,4 +1,23 @@
 package chapter9;
 
-public class Employee {
+public class Employee extends Person{
+    
+    private String employeeID;
+    private String title;
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
