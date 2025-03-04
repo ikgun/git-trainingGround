e4580501ab75cbe67d8631a5bc9e0568afc34fe4
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 11:37:45 +0100

    Completed Product interface

--- java_course/src/chapter11/Product.java
+++ java_course/src/chapter11/Product.java
@@ -1,4 +1,13 @@
 package chapter11;
 
 public interface Product {
+
+    double getPrice();
+    void setPrice();
+
+    String getName();
+    void setName();
+
+    String getColor();
+    void setColor(String color);
 }
