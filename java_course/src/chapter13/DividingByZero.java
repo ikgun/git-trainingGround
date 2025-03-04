b62d99432dfc5a20a4e5f735d45b11415649cef9
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 15:55:49 +0100

    Completed chapter 13 exercise

--- java_course/src/chapter13/DividingByZero.java
+++ java_course/src/chapter13/DividingByZero.java
@@ -1,4 +1,15 @@
 package chapter13;
 
 public class DividingByZero {
+
+    public static void main(String[] args) {
+       try {
+           int c = 30/0;
+       } catch (ArithmeticException e) {
+           e.printStackTrace();
+           System.out.println("Error: divided by zero.");
+       }finally {
+           System.out.println("Division is fun!");
+       }
+    }
 }
