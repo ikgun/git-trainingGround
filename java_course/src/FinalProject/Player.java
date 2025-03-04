3ddc81e2a61b1f26e33ac16ed9582c52a8378c79
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 16:17:12 +0100

    Updated Player class

--- java_course/src/FinalProject/Player.java
+++ java_course/src/FinalProject/Player.java
@@ -1,4 +1,27 @@
 package FinalProject;
 
 public class Player {
+
+    private String name;
+    private String guess;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
 }
