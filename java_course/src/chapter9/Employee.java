1cd1857feafb138bc04c5cda10e65b0175e33dd3
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 09:57:40 +0100

    Added constructor to the Employee class

--- java_course/src/chapter9/Employee.java
+++ java_course/src/chapter9/Employee.java
@@ -5,6 +5,10 @@ public class Employee extends Person{
     private String employeeID;
     private String title;
 
+    public Employee() {
+        System.out.println("In Employee defualt constructor.");
+    }
+
     public String getEmployeeID() {
         return employeeID;
     }
--- java_course/src/chapter9/InheritanceTester.java
+++ java_course/src/chapter9/InheritanceTester.java
@@ -3,6 +3,7 @@ package chapter9;
 public class InheritanceTester {
 
     public static void main(String[] args) {
+
         Person person = new Person();
         Employee employee = new Employee();
 
