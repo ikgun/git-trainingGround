d4ebd7e05f3c99ae7cb32b55c55f557a9bd0a53f
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 17:09:30 +0100

    Completed final project

--- java_course/src/FinalProject/Coin.java
+++ java_course/src/FinalProject/Coin.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Coin {
 
     private String side;
-    private static final String HEADS = "Heads";
-    private static final String TAILS = "Tails";
+    public static final String HEADS = "Heads";
+    public static final String TAILS = "Tails";
 
     public void flip() {
         Random number = new Random();
