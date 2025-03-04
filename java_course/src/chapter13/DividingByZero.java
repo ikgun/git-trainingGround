654624b111a997827c17a8d68c6c782b5adc4b0a
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 16:14:04 +0100

    Updated Coin class

--- java_course/src/chapter13/DividingByZero.java
+++ java_course/src/chapter13/DividingByZero.java
@@ -3,13 +3,13 @@ package chapter13;
 public class DividingByZero {
 
     public static void main(String[] args) {
-       try {
-           int c = 30/0;
-       } catch (ArithmeticException e) {
-           e.printStackTrace();
-           System.out.println("Error: divided by zero.");
-       }finally {
-           System.out.println("Division is fun!");
-       }
+        try {
+            int c = 30 / 0;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            System.out.println("Error: divided by zero.");
+        } finally {
+            System.out.println("Division is fun!");
+        }
     }
 }
