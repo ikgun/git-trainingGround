20d0a55a4769bd67ecc065a672ee8e2802c4cd5f
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-04 10:30:57 +0100

    Updated the Cake class

--- java_course/src/chapter9/Cake.java
+++ java_course/src/chapter9/Cake.java
@@ -1,4 +1,15 @@
 package chapter9;
 
 public class Cake {
+
+    protected String flavor;
+    protected double price;
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
